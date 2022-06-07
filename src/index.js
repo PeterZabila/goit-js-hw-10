13f739a2925159878570cda22fad0ebe1caebdbc
ZABILA PETRO <petrozabila@gmail.com>
2022-06-07 11:42:33 +0300

    added debounce

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/styles.css';
 import fetchCountries from './fetchCountries';
-import debounce from 'lodash.debounce'
+import debounce from "lodash.debounce"
 
 const DEBOUNCE_DELAY = 300;
 
