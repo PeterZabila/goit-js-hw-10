e7b64621db42f5766206ebc4894e8452d0f2d3a4
ZABILA PETRO <petrozabila@gmail.com>
2022-06-07 16:02:58 +0300

    3 task got country

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import {infoHandler} from './index';
+export { fetchCountries };
 
+ function fetchCountries(name) {
+   if(name !== "") {
+    // fetch(`https://restcountries.com/v2/name/${name}`)
+    // fetch(`https://restcountries.com/v2/{service}?fields=${field},${field},${field}`)
+    fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags.svg,languages`)
+        .then(response => {
+            return response.json();
+        })
+        .then(object => {
+            console.log(object);
+            Notify.success('✅ Fulfilled');
 
-export default function fetchCountries(name) {
+            object.map(el => infoHandler(el))
+           
+        })
+       .catch(error => console.log(error))
+    }
+//         // .then((parsedObject) => {
+//         //     Notify.success(`✅ Fulfilled promise ${parsedObject} `);
+//         //     infoHandler(name);
+//         // })
+//         .catch(() => {
+//             console.log("Not found any");
+//             Notify.failure(`❌ Rejected promise `);
+//         })
+//    }; 
+        // }
     
-    fetch("https://restcountries.com/v3.1/name/{name}").then(result => console.log(result)).catch(console.log("Not found"))
 };
+
+
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/styles.css';
-import fetchCountries from './fetchCountries';
-import debounce from "lodash.debounce"
+import { fetchCountries } from './fetchCountries';
+import debounce from "lodash.debounce";
+
+export { infoHandler };
 
 const DEBOUNCE_DELAY = 300;
 
@@ -11,6 +13,26 @@ const refs = {
     info: document.querySelector('.country-info'),
 };
 
-let name = refs.input.value;
+function inputHandler () {
+    const inputValue = refs.input.value;
+    fetchCountries(inputValue);
+}
 
-refs.input.addEventListener('input', debounce(fetchCountries, DEBOUNCE_DELAY));
+function infoHandler({name, capital, population, flags, languages}) {
+    // refs.info.classList.remove('is-hidden');
+    refs.info.insertAdjacentHTML('afterbegin', 
+    `<p class="info">Capital: ${capital}</p
+    <p class="info">Population: ${population}</p>
+    <h3 class="country"><img src="${flags}" alt="flag" width="30px">${name}</h3>
+    <p class="info">Languages: ${(Object.values(languages)).toString()}</p>`
+
+    
+    // `<h3 class="country"><img src="${flags.svg}" alt="flag" width="30px">${name.official}</h3>
+    // <p class="info">Capital: ${capital}</p>
+    // <p class="info">Population: ${population}</p>
+    // <p class="info">Languages: ${Object.values(languages)}</p>`
+    )
+    console.log(typeof(Object.values(languages)));
+}
+
+refs.input.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
